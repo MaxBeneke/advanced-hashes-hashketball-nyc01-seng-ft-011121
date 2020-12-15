c5cc08f01c5071bccd9f6fe474d00f8951a6ac94
Max Beneke <max.beneke@gmail.com>
2020-12-15 15:06:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,7 +182,7 @@ def player_numbers(team)
   game_hash.each do |location, team_data|
     
     if team_data[:team_name] == team
-     team_data.each do |team_info, data|
+     team_data[:players].each do |team_info, data|
        binding.pry
     end
     end
